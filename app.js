ad54e58f2465821bfcd7498b271bc0a60d0b8e11
LeonardoR998 <leonardoricci839@gmail.com>
2024-11-21 16:56:41 +0100

    add milestone 2 and fix name and file

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
+// app.js
+
 const express = require("express");
 const app = express();
+
+// Imposto la porta
 const port = 3000;
 
-// Asset statico
-app.use("/images", express.static("public/images"));
+app.use(express.json());
 
+// Importo le rotte
 const postsRouter = require("./routers/posts");
 
+// Imposto il routing con il router delle rotte dei post
 app.use("/", postsRouter);
 
+// Avvio il server sulla porta specificata
 app.listen(port, () => {
   console.log(`Server in esecuzione sulla porta ${port}`);
 });
